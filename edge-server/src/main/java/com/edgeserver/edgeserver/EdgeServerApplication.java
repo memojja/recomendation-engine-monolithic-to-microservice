43eaa6f5e27854864bcac0b08ad4bded5d094cf8
mehmet <memojja1907@gmail.com>
2018-05-06 00:10:20 +0300

    update edge server and problem fixed

--- edge-server/src/main/java/com/edgeserver/edgeserver/EdgeServerApplication.java
+++ edge-server/src/main/java/com/edgeserver/edgeserver/EdgeServerApplication.java
@@ -2,11 +2,14 @@ package com.edgeserver.edgeserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
+
 public class EdgeServerApplication {
 
 
